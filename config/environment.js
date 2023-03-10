f36873d40fb349926b600e5ef12638b783800ac6
Lias Kleisa <kleisa@puzzle.ch>
2023-03-10 11:42:04 +0100

    Replace ember-font-awesome with @fortawesome/ember-fontawesome

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,7 @@
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return { };
+module.exports = function() {
+  return {
+    'free-solid-svg-icons': ['star', 'star-o'],
+  };
 };
